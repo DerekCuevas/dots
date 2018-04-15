d98c31c805fb3cc31eb51eba96487404b03c1d44
Derek Cuevas <dccuevas2@gmail.com>
2018-04-15 11:21:51 -0700

    elm css

--- src/Main.elm
+++ src/Main.elm
@@ -1,29 +1,36 @@
 module Main exposing (main)
 
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (..)
+import Html
+import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (..)
+import Html.Styled.Events exposing (..)
+import Css
+import Css.Colors
+import Color exposing (Color)
 import Time exposing (Time)
 import Random exposing (Generator)
-import Color exposing (Color)
-import Array
+import Array exposing (Array)
 
 
 -- CONFIG
 
 
+dotCount : Int
 dotCount =
     99
 
 
+dotWidth : Float
 dotWidth =
     150
 
 
+updateDotsInterval : Time
 updateDotsInterval =
-    Time.second * 2
+    Time.second
 
 
+colors : Array Color
 colors =
     Array.fromList
         [ Color.lightRed
@@ -40,6 +47,7 @@ colors =
 -- RANDOM GENERATORS
 
 
+randomColorGenerator : Generator Color
 randomColorGenerator =
     Random.int 0 (Array.length colors - 1)
         |> Random.map
@@ -50,10 +58,12 @@ randomColorGenerator =
             )
 
 
+dotsGenerator : Generator (List Color)
 dotsGenerator =
     Random.list dotCount randomColorGenerator
 
 
+borderRadiusGenerator : Generator Float
 borderRadiusGenerator =
     Random.float 0 (dotWidth / 2)
 
@@ -62,14 +72,17 @@ borderRadiusGenerator =
 -- COMMANDS
 
 
+generateDots : Cmd Msg
 generateDots =
     Random.generate SetDots dotsGenerator
 
 
+generateBorderRadius : Cmd Msg
 generateBorderRadius =
     Random.generate SetBorderRadius borderRadiusGenerator
 
 
+generate : List (Cmd Msg)
 generate =
     [ generateDots
     , generateBorderRadius
@@ -80,6 +93,16 @@ generate =
 -- MODEL
 
 
+type alias Model =
+    { isTicking : Bool
+    , time : Maybe Time
+    , totalTicks : Int
+    , dots : List Color
+    , borderRadius : Float
+    }
+
+
+init : ( Model, Cmd Msg )
 init =
     { isTicking = True
     , time = Nothing
@@ -101,6 +124,7 @@ type Msg
     | SetBorderRadius Float
 
 
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         ToggleIsTicking ->
@@ -124,6 +148,7 @@ update msg model =
 -- SUBSCRIPTIONS
 
 
+subscriptions : Model -> Sub Msg
 subscriptions model =
     if model.isTicking then
         Time.every updateDotsInterval Tick
@@ -135,6 +160,7 @@ subscriptions model =
 -- VIEW
 
 
+view : Model -> Html Msg
 view model =
     let
         time =
@@ -142,9 +168,9 @@ view model =
                 |> Maybe.map toString
                 |> Maybe.withDefault "N/A"
     in
-        div [ style [ ( "backgroundColor", "black" ) ] ]
+        div [ css [ Css.backgroundColor Css.Colors.black ] ]
             [ div []
-                [ div [ style [ ( "display", "flex" ), ( "flex-wrap", "wrap" ) ] ] <|
+                [ div [ css [ Css.displayFlex, Css.flexWrap Css.wrap ] ] <|
                     List.map (viewDot model.borderRadius) model.dots
                 ]
             , button [ onClick ToggleIsTicking ]
@@ -158,52 +184,37 @@ view model =
             ]
 
 
-colorToString color =
-    let
-        { red, green, blue, alpha } =
-            Color.toRgb color
-    in
-        "rgba("
-            ++ toString red
-            ++ ", "
-            ++ toString green
-            ++ ", "
-            ++ toString blue
-            ++ ", "
-            ++ toString alpha
-            ++ ")"
-
-
-toPx x =
-    toString x ++ "px"
-
-
+viewDot : Float -> Color -> Html Msg
 viewDot borderRadius color =
     let
         updateDotsIntervalInSeconds =
             Time.inSeconds updateDotsInterval
 
-        viewStyle =
-            style
-                [ ( "backgroundColor", colorToString color )
-                , ( "width", toPx dotWidth )
-                , ( "height", toPx dotWidth )
-                , ( "border-radius", toPx borderRadius )
-                , ( "margin", "4px" )
-                , ( "transition", "all " ++ toString updateDotsIntervalInSeconds ++ "s ease-out" )
-                ]
+        { red, green, blue, alpha } =
+            Color.toRgb color
     in
-        div [ viewStyle ] []
+        div
+            [ css
+                [ Css.backgroundColor (Css.rgba red green blue alpha)
+                , Css.width (Css.px dotWidth)
+                , Css.height (Css.px dotWidth)
+                , Css.borderRadius (Css.px borderRadius)
+                , Css.margin (Css.px 4)
+                ]
+            , style [ ( "transition", "all " ++ toString updateDotsIntervalInSeconds ++ "s ease-out" ) ]
+            ]
+            []
 
 
 
 -- PROGRAM
 
 
+main : Program Never Model Msg
 main =
     Html.program
         { init = init
         , update = update
-        , view = view
+        , view = view >> toUnstyled
         , subscriptions = subscriptions
         }
